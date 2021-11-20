a65ecb484b71c7a178731c6659aeefe04ece13a9
Zemi <buivanloi.2010@gmail.com>
2021-11-20 17:38:19 +0700

    remove style old theme

--- wp-content/themes/hello-elementor/index.php
+++ wp-content/themes/hello-elementor/index.php
@@ -315,48 +315,34 @@ if ( is_singular() ) {
         </div>
         <div class="row mt-4">
             <div class="col-md-4 mb-3">
-                <figure class="card imghvr-slide-up card-covid"><img class="card-img"
-                                                                     src="data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%201%201'%3E%3C/svg%3E"
-                                                                     data-lazy-src="https://dai-ichi.vn/wp-content/uploads/2021/06/Chuong-trinh-ho-tro-covid19-het-thang-9.jpg"
-                                                                     width="100%" alt=""/>
-                    <noscript><img class="card-img"
-                                   src="https://dai-ichi.vn/wp-content/uploads/2021/06/Chuong-trinh-ho-tro-covid19-het-thang-9.jpg"
-                                   width="100%" alt=""/></noscript>
-                    <a href="https://dai-ichi.vn/thong-bao-ho-tro-dac-biet-khach-hang-covid-19/">
-                        <figcaption class="d-none-hv"><img
-                                    src="data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%201%201'%3E%3C/svg%3E"
-                                    data-lazy-src="https://dai-ichi.vn/wp-content/uploads/2020/11/multiple-users-silhouette.png"
-                                    width="50px" alt="">
-                            <noscript><img
-                                        src="https://dai-ichi.vn/wp-content/uploads/2020/11/multiple-users-silhouette.png"
-                                        width="50px" alt=""></noscript>
+                <figure class="card imghvr-slide-up card-covid">
+                    <img class="card-img"
+                         src="https://dai-ichi.vn/wp-content/uploads/2021/06/Chuong-trinh-ho-tro-covid19-het-thang-9.jpg"
+                         width="100%" alt=""/>
+                    <a href="javascript:void(0)">
+                        <figcaption class="d-none-hv">
+                            <img src="https://dai-ichi.vn/wp-content/uploads/2020/11/multiple-users-silhouette.png" width="50px" alt="">
                             <h3>DAI-ICHI LIFE ĐỒNG HÀNH CÙNG QUÝ KHÁCH HÀNG CÙNG VƯỢT QUA DỊCH COVID</h3>
                             <p>Ngày 7/6/2021, Dai-ichi Việt Nam đã ra thông báo chính thức - số 05/OPS/07062021.Về việc:
                                 Kéo dài chương trình hỗ trợ dành cho khách hàng liên quan đến COVID19 đến hết thang
-                                9/2021 hoặc khi có thông báo mới nhất.</p></figcaption>
-                    </a></figure>
+                                9/2021 hoặc khi có thông báo mới nhất.</p>
+                        </figcaption>
+                    </a>
+                </figure>
             </div>
             <div class="col-md-4 mb-3">
-                <figure class="card imghvr-slide-up card-covid"><img class="card-img"
-                                                                     src="data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%201%201'%3E%3C/svg%3E"
-                                                                     data-lazy-src="https://dai-ichi.vn/wp-content/uploads/2021/01/chao-xua-tan-suu.png"
-                                                                     width="100%" alt=""/>
-                    <noscript><img class="card-img"
-                                   src="https://dai-ichi.vn/wp-content/uploads/2021/01/chao-xua-tan-suu.png"
-                                   width="100%" alt=""/></noscript>
-                    <a href="https://dai-ichi.vn/tin-tuc-khuyen-mai/">
-                        <figcaption><img
-                                    src="data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%201%201'%3E%3C/svg%3E"
-                                    data-lazy-src="https://dai-ichi.vn/wp-content/uploads/2020/11/multiple-users-silhouette.png"
-                                    width="50px" alt="">
-                            <noscript><img
-                                        src="https://dai-ichi.vn/wp-content/uploads/2020/11/multiple-users-silhouette.png"
-                                        width="50px" alt=""></noscript>
+                <figure class="card imghvr-slide-up card-covid">
+                    <img class="card-img" src="https://dai-ichi.vn/wp-content/uploads/2021/01/chao-xua-tan-suu.png" width="100%" alt=""/>
+                    <a href="/tin-tuc-khuyen-mai/">
+                        <figcaption>
+                            <img src="https://dai-ichi.vn/wp-content/uploads/2020/11/multiple-users-silhouette.png" width="50px" alt=""></noscript>
                             <h3>TÂN SỬU XUÂN SANG - NGẬP TRÀN QUÀ TẶNG</h3>
                             <p>Nhận ngay nhiều phần quà hấp dẫn cùng Dai-ichi khi khách hàng tham gia kỳ phí đầu tiên
                                 <br> Chương trình khuyến mại dành cho Quý Khách hàng tham gia bảo hiểm thông qua tư vấn
-                                bảo hiểm làm việc tại phòng kinh doanh HN214 - Dai-ichi Hà Nội..."</p></figcaption>
-                    </a></figure>
+                                bảo hiểm làm việc tại phòng kinh doanh HN214 - Dai-ichi Hà Nội..."</p>
+                        </figcaption>
+                    </a>
+                </figure>
             </div>
             <div class="col-md-4 mb-3">
                 <figure class="card imghvr-slide-up card-covid"><img class="card-img"
