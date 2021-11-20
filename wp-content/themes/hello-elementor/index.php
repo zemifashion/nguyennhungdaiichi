62d93f8ae895c7f3ce18a4613714b9adc03dea91
Zemi <buivanloi.2010@gmail.com>
2021-11-20 19:13:10 +0700

    xoa code ko su dung

--- wp-content/themes/hello-elementor/index.php
+++ wp-content/themes/hello-elementor/index.php
@@ -3,8 +3,7 @@
 <head>
     <meta name="robots" content="follow, index"/>
     <title>Trang Chủ - Dai-ichi HN214</title>
-    <meta name="description"
-          content="Trang chủ chính thức của Phòng Kinh Doanh Hà Nội 214 - công ty TNHH Bảo Hiểm Nhân Thọ Dai-ichi Việt Nam. Tư vấn, giải đáp thắc mắc cho khách hàng về Dai-ichi"/>
+    <meta name="description" content="Trang chủ chính thức của Phòng Kinh Doanh Hà Nội 214 - công ty TNHH Bảo Hiểm Nhân Thọ Dai-ichi Việt Nam. Tư vấn, giải đáp thắc mắc cho khách hàng về Dai-ichi"/>
     <meta name="robots" content="follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:large"/>
     <link rel="canonical" href="https://dai-ichi.vn/"/>
     <meta property="og:locale" content="vi_VN"/>
@@ -17,113 +16,10 @@
     <meta property="og:updated_time" content="2021-10-10T00:27:29+07:00"/>
     <meta name="twitter:card" content="summary_large_image"/>
     <meta name="twitter:title" content="Trang Chủ - Dai-ichi HN214"/>
-    <meta name="twitter:description"
-          content="Trang chủ chính thức của Phòng Kinh Doanh Hà Nội 214 - công ty TNHH Bảo Hiểm Nhân Thọ Dai-ichi Việt Nam. Tư vấn, giải đáp thắc mắc cho khách hàng về Dai-ichi"/>
-    <script type="application/ld+json" class="rank-math-schema">{
-            "@context": "https://schema.org",
-            "@graph": [
-                {
-                    "@type": [
-                        "Person",
-                        "Organization"
-                    ],
-                    "@id": "https://dai-ichi.vn/#person",
-                    "name": "Nguy\u1ec5n Ti\u1ebfn",
-                    "logo": {
-                        "@type": "ImageObject",
-                        "@id": "https://dai-ichi.vn/#logo",
-                        "url": "https://dai-ichi.vn/wp-content/uploads/2021/01/cropped-1.png",
-                        "caption": "Nguy\u1ec5n Ti\u1ebfn",
-                        "inLanguage": "vi",
-                        "width": "300",
-                        "height": "200"
-                    },
-                    "image": {
-                        "@type": "ImageObject",
-                        "@id": "https://dai-ichi.vn/#logo",
-                        "url": "https://dai-ichi.vn/wp-content/uploads/2021/01/cropped-1.png",
-                        "caption": "Nguy\u1ec5n Ti\u1ebfn",
-                        "inLanguage": "vi",
-                        "width": "300",
-                        "height": "200"
-                    }
-                },
-                {
-                    "@type": "WebSite",
-                    "@id": "https://dai-ichi.vn/#website",
-                    "url": "https://dai-ichi.vn",
-                    "name": "Nguy\u1ec5n Ti\u1ebfn",
-                    "publisher": {
-                        "@id": "https://dai-ichi.vn/#person"
-                    },
-                    "inLanguage": "vi",
-                    "potentialAction": {
-                        "@type": "SearchAction",
-                        "target": "https://dai-ichi.vn/?s={search_term_string}",
-                        "query-input": "required name=search_term_string"
-                    }
-                },
-                {
-                    "@type": "Person",
-                    "@id": "https://dai-ichi.vn/author/aia_admin/",
-                    "name": "Nguy\u1ec5n Ti\u1ebfn",
-                    "url": "https://dai-ichi.vn/author/aia_admin/",
-                    "image": {
-                        "@type": "ImageObject",
-                        "@id": "https://secure.gravatar.com/avatar/61beb94e2fa8e1ebf6ffc6ac3cca7049?s=96&amp;d=mm&amp;r=g",
-                        "url": "https://secure.gravatar.com/avatar/61beb94e2fa8e1ebf6ffc6ac3cca7049?s=96&amp;d=mm&amp;r=g",
-                        "caption": "Nguy\u1ec5n Ti\u1ebfn",
-                        "inLanguage": "vi"
-                    },
-                    "sameAs": [
-                        "https://dai-ichi.vn/"
-                    ]
-                },
-                {
-                    "@type": "WebPage",
-                    "@id": "https://dai-ichi.vn/#webpage",
-                    "url": "https://dai-ichi.vn/",
-                    "name": "Trang Ch\u1ee7 - Dai-ichi HN214",
-                    "datePublished": "2020-10-17T04:21:04+07:00",
-                    "dateModified": "2021-10-10T00:27:29+07:00",
-                    "author": {
-                        "@id": "https://dai-ichi.vn/author/aia_admin/"
-                    },
-                    "isPartOf": {
-                        "@id": "https://dai-ichi.vn/#website"
-                    },
-                    "inLanguage": "vi"
-                },
-                {
-                    "@type": "Article",
-                    "headline": "Trang Ch\u1ee7 - Dai-ichi HN214",
-                    "keywords": "trang ch\u1ee7",
-                    "datePublished": "2020-10-17T04:21:04+07:00",
-                    "dateModified": "2021-10-10T00:27:29+07:00",
-                    "author": {
-                        "@id": "https://dai-ichi.vn/author/aia_admin/"
-                    },
-                    "publisher": {
-                        "@id": "https://dai-ichi.vn/#person"
-                    },
-                    "description": "Trang ch\u1ee7 ch\u00ednh th\u1ee9c c\u1ee7a Ph\u00f2ng Kinh Doanh H\u00e0 N\u1ed9i 214 - c\u00f4ng ty TNHH B\u1ea3o Hi\u1ec3m Nh\u00e2n Th\u1ecd Dai-ichi Vi\u1ec7t Nam. T\u01b0 v\u1ea5n, gi\u1ea3i \u0111\u00e1p th\u1eafc m\u1eafc cho kh\u00e1ch h\u00e0ng v\u1ec1 Dai-ichi",
-                    "name": "Trang Ch\u1ee7 - Dai-ichi HN214",
-                    "@id": "https://dai-ichi.vn/#richSnippet",
-                    "isPartOf": {
-                        "@id": "https://dai-ichi.vn/#webpage"
-                    },
-                    "inLanguage": "vi",
-                    "mainEntityOfPage": {
-                        "@id": "https://dai-ichi.vn/#webpage"
-                    }
-                }
-            ]
-        }</script>
-    <link rel="alternate" type="application/rss+xml" title="Dòng thông tin Dai-ichi HN214 &raquo;"
-          href="https://dai-ichi.vn/feed/"/>
-    <link rel="alternate" type="application/rss+xml" title="Dòng phản hồi Dai-ichi HN214 &raquo;"
-          href="https://dai-ichi.vn/comments/feed/"/>
-    <style>img.wp-smiley, img.emoji {
+    <meta name="twitter:description" content="Trang chủ chính thức của Phòng Kinh Doanh Hà Nội 214 - công ty TNHH Bảo Hiểm Nhân Thọ Dai-ichi Việt Nam. Tư vấn, giải đáp thắc mắc cho khách hàng về Dai-ichi"/>
+
+    <style>
+        img.wp-smiley, img.emoji {
             display: inline !important;
             border: none !important;
             box-shadow: none !important;
@@ -133,7 +29,8 @@
             vertical-align: -0.1em !important;
             background: none !important;
             padding: 0 !important
-        }</style>
+        }
+    </style>
     <link rel='stylesheet' id='wp-block-library-css' href='<?= get_template_directory_uri() ?>/css/style.min.css' media='all'/>
     <link rel='stylesheet' id='contact-form-7-css' href='<?= get_template_directory_uri() ?>/css/styles-5.5.2.css' media='all'/>
     <link rel='stylesheet' id='sanpop-public-style-css' href='<?= get_template_directory_uri() ?>/css/sanpop-public-style-1.1.1.css' media='all'/>
@@ -208,33 +105,7 @@
     <link rel="wlwmanifest" type="application/wlwmanifest+xml" href="https://dai-ichi.vn/wp-includes/wlwmanifest.xml"/>
     <meta name="generator" content="WordPress 5.7.4"/>
     <link rel='shortlink' href='https://dai-ichi.vn/'/>
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-190156923-1"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
 
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-
-        gtag('js', new Date());
-
-        gtag('config', 'UA-190156923-1');</script>
-    <script>(function (w, d, s, l, i) {
-            w[l] = w[l] || [];
-            w[l].push({
-                'gtm.start':
-                    new Date().getTime(), event: 'gtm.js'
-            });
-            var f = d.getElementsByTagName(s)[0],
-                j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
-            j.async = true;
-            j.src =
-                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-            f.parentNode.insertBefore(j, f);
-        })(window, document, 'script', 'dataLayer', 'GTM-5H9XQ3Z');</script>
-    <script>var ms_grabbing_curosr = 'https://dai-ichi.vn/wp-content/plugins/master-slider/public/assets/css/common/grabbing.cur',
-            ms_grab_curosr = 'https://dai-ichi.vn/wp-content/plugins/master-slider/public/assets/css/common/grab.cur';</script>
-    <meta name="generator" content="MasterSlider 3.7.10 - Responsive Touch Image Slider | avt.li/msf"/>
     <style>
         .recentcomments a {
             display: inline !important;
@@ -242,19 +113,10 @@
             margin: 0 !important
         }
     </style>
-    <link rel="icon" href="https://dai-ichi.vn/wp-content/uploads/2021/01/cropped-to3rypcleg9nzytr3gpl-32x32.png"
-          sizes="32x32"/>
-    <link rel="icon" href="https://dai-ichi.vn/wp-content/uploads/2021/01/cropped-to3rypcleg9nzytr3gpl-192x192.png"
-          sizes="192x192"/>
-    <link rel="apple-touch-icon"
-          href="https://dai-ichi.vn/wp-content/uploads/2021/01/cropped-to3rypcleg9nzytr3gpl-180x180.png"/>
-    <meta name="msapplication-TileImage"
-          content="https://dai-ichi.vn/wp-content/uploads/2021/01/cropped-to3rypcleg9nzytr3gpl-270x270.png"/>
-    <noscript>
-        <style id="rocket-lazyload-nojs-css">.rll-youtube-player, [data-lazy-src] {
-                display: none !important
-            }</style>
-    </noscript>
+    <link rel="icon" href="https://dai-ichi.vn/wp-content/uploads/2021/01/cropped-to3rypcleg9nzytr3gpl-32x32.png" sizes="32x32"/>
+    <link rel="icon" href="https://dai-ichi.vn/wp-content/uploads/2021/01/cropped-to3rypcleg9nzytr3gpl-192x192.png" sizes="192x192"/>
+    <link rel="apple-touch-icon" href="https://dai-ichi.vn/wp-content/uploads/2021/01/cropped-to3rypcleg9nzytr3gpl-180x180.png"/>
+    <meta name="msapplication-TileImage" content="https://dai-ichi.vn/wp-content/uploads/2021/01/cropped-to3rypcleg9nzytr3gpl-270x270.png"/>
     <meta name="robots" content="follow, index"/>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -303,107 +165,26 @@ if ( is_singular() ) {
 <?php get_footer(); ?>
 <nav id="menu-mobile">
     <ul id="menu-menu-1" class="">
-        <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-498"><a
-                    href="/gioi-thieu/">Giới Thiệu</a></li>
-        <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-449"><a
-                    href="/san-pham/">Sản phẩm</a></li>
-        <li class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-401"><a
-                    href="/category/dich-vu-khach-hang/">DỊCH VỤ KHÁCH HÀNG</a></li>
-        <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-22"><a
-                    href="/tin-tuc-khuyen-mai/">Tin tức</a></li>
-        <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-23"><a
-                    href="/tuyen-dung/">Tuyển dụng</a></li>
+        <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-498">
+            <a href="/gioi-thieu/">Giới Thiệu</a>
+        </li>
+        <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-449">
+            <a href="/san-pham/">Sản phẩm</a>
+        </li>
+        <li class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-401">
+            <a href="/category/dich-vu-khach-hang/">DỊCH VỤ KHÁCH HÀNG</a>
+        </li>
+        <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-22">
+            <a href="/tin-tuc-khuyen-mai/">Tin tức</a>
+        </li>
+        <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-23">
+            <a href="/tuyen-dung/">Tuyển dụng</a>
+        </li>
     </ul>
 </nav>
-<script src='<?= get_template_directory_uri() ?>/js/wp-polyfill.min-7.4.4.js' id='wp-polyfill-js'></script>
-<script id='wp-polyfill-js-after'>('fetch' in window) || document.write('<script src="<?= get_template_directory_uri() ?>/js/wp-polyfill-fetch.min-3.0.0.js"></scr' + 'ipt>');
-    (document.contains) || document.write('<script src="<?= get_template_directory_uri() ?>/js/wp-polyfill-node-contains.min-3.42.0.js"></scr' + 'ipt>');
-    (window.DOMRect) || document.write('<script src="<?= get_template_directory_uri() ?>/js/wp-polyfill-dom-rect.min-3.42.0.js"></scr' + 'ipt>');
-    (window.URL && window.URL.prototype && window.URLSearchParams) || document.write('<script src="<?= get_template_directory_uri() ?>/js/wp-polyfill-url.min-3.6.4.js"></scr' + 'ipt>');
-    (window.FormData && window.FormData.prototype.keys) || document.write('<script src="<?= get_template_directory_uri() ?>/js/wp-polyfill-formdata.min-3.0.12.js"></scr' + 'ipt>');
-    (Element.prototype.matches && Element.prototype.closest) || document.write('<script src="<?= get_template_directory_uri() ?>/js/wp-polyfill-element-closest.min-2.0.2.js"></scr' + 'ipt>');
-    ('objectFit' in document.documentElement.style) || document.write('<script src="<?= get_template_directory_uri() ?>/js/wp-polyfill-object-fit.min-2.3.4.js"></scr' + 'ipt>');</script>
-<script id='contact-form-7-js-extra'>
-    var wpcf7 = {
-        "api": {
-            "root": "https:\/\/dai-ichi.vn\/wp-json\/",
-            "namespace": "contact-form-7\/v1"
-        }, "cached": "1"
-    };
-</script>
-<script src='<?= get_template_directory_uri() ?>/js/index-5.5.2.js' id='contact-form-7-js'></script>
+
 <script src='<?= get_template_directory_uri() ?>/js/navigation-1.0.0.js' id='aia-navigation-js'></script>
-<script>window.lazyLoadOptions = {
-        elements_selector: "img[data-lazy-src],.rocket-lazyload,iframe[data-lazy-src]",
-        data_src: "lazy-src",
-        data_srcset: "lazy-srcset",
-        data_sizes: "lazy-sizes",
-        skip_invisible: false,
-        class_loading: "lazyloading",
-        class_loaded: "lazyloaded",
-        threshold: 300,
-        callback_load: function (element) {
-            if (element.tagName === "IFRAME" && element.dataset.rocketLazyload == "fitvidscompatible") {
-                if (element.classList.contains("lazyloaded")) {
-                    if (typeof window.jQuery != "undefined") {
-                        if (jQuery.fn.fitVids) {
-                            jQuery(element).parent().fitVids();
-                        }
-                    }
-                }
-            }
-        }
-    };
 
-    // Listen to the Initialized event
-    window.addEventListener('LazyLoad::Initialized', function (e) {
-        // Get the instance and puts it in the lazyLoadInstance variable
-        var lazyLoadInstance = e.detail.instance;
-
-        if (window.MutationObserver) {
-            var observer = new MutationObserver(function (mutations) {
-                mutations.forEach(function (mutation) {
-                    mutation.addedNodes.forEach(function (node) {
-                        if (typeof node.getElementsByTagName !== 'function') {
-                            return;
-                        }
-
-                        imgs = node.getElementsByTagName('img');
-                        iframes = node.getElementsByTagName('iframe');
-                        rocket_lazy = node.getElementsByClassName('rocket-lazyload');
-
-                        if (0 === imgs.length && 0 === iframes.length && 0 === rocket_lazy.length) {
-                            return;
-                        }
-
-                        lazyLoadInstance.update();
-                    });
-                });
-            });
-
-            var b = document.getElementsByTagName("body")[0];
-            var config = {childList: true, subtree: true};
-
-            observer.observe(b, config);
-        }
-    }, false);</script>
-<script data-no-minify="1" async src="<?= get_template_directory_uri() ?>/js/lazyload.min.js"></script>
-<script>function lazyLoadThumb(e) {
-        var t = '<img data-lazy-src="https://i.ytimg.com/vi/ID/hqdefault.jpg" width="480" height="360"><noscript><img src="https://i.ytimg.com/vi/ID/hqdefault.jpg" width="480" height="360"></noscript>',
-            a = '<div class="play"></div>';
-        return t.replace("ID", e) + a
-    }
-
-    function lazyLoadYoutubeIframe() {
-        var e = document.createElement("iframe"), t = "https://www.youtube.com/embed/ID?autoplay=1";
-        t += 0 === this.dataset.query.length ? '' : '&' + this.dataset.query;
-        e.setAttribute("src", t.replace("ID", this.dataset.id)), e.setAttribute("frameborder", "0"), e.setAttribute("allowfullscreen", "1"), this.parentNode.replaceChild(e, this)
-    }
-
-    document.addEventListener("DOMContentLoaded", function () {
-        var e, t, a = document.getElementsByClassName("rll-youtube-player");
-        for (t = 0; t < a.length; t++) e = document.createElement("div"), e.setAttribute("data-id", a[t].dataset.id), e.setAttribute("data-query", a[t].dataset.query), e.innerHTML = lazyLoadThumb(a[t].dataset.id), e.onclick = lazyLoadYoutubeIframe, a[t].appendChild(e)
-    });</script>
 <script type="text/javascript" src="<?= get_template_directory_uri() ?>/js/jquery-3.5.1.js"></script>
 <script type="text/javascript" src="<?= get_template_directory_uri() ?>/js/bootstrap.js"></script>
 <script type="text/javascript" src="<?= get_template_directory_uri() ?>/js/jquery.mmenu.all.min.js"></script>
@@ -427,16 +208,7 @@ if ( is_singular() ) {
         }
     }
 </script>
-<script type="text/javascript">
-    var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-    (function () {
-        var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-        s1.async = true;
-        s1.src = 'https://embed.tawk.to/600e4cf6a9a34e36b96fdf80/1esrtp99u';
-        s1.charset = 'UTF-8';
-        s1.setAttribute('crossorigin', '*');
-        s0.parentNode.insertBefore(s1, s0);
-    })();</script>
+
 </body>
 </html>
 <script>
@@ -533,4 +305,3 @@ if ( is_singular() ) {
     }
     h1.text-uppercase > p {color: #ff0000;}
 </style>
-<!-- This website is like a Rocket, isn't it? Performance optimized by WP Rocket. Learn more: https://wp-rocket.me - Debug: cached@1637326775 -->
